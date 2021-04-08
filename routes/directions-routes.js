26ef2a267c2107728190f9bbd95fb77d3334f091
hind benazzi <hind.benazzi@edu.uca.ma>
2021-04-08 10:23:42 +0200

    places api

--- routes/directions-routes.js
+++ routes/directions-routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const {getDirections
       } = require('../controllers/directionsController');
-
+const {getPlaces
+      } = require('../controllers/placesController');
 const router = express.Router();
 
 router.post('/direction', getDirections);
-
-
+router.get('/places/:name', getPlaces);
 module.exports = {
     routes: router
 }
