1df540e6af794431b8b2dc9fa724bcdb46d66344
zakaria011 <maskanzakaria@gmail.com>
2021-04-14 00:49:19 +0000

    geocodeversion

--- controllers/directionsController.js
+++ controllers/directionsController.js
@@ -7,19 +7,71 @@ const firestore = firebase.firestore();
 const Steps = require('../models/steps');
 const request = require('request');
 const url = "https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyBz6IDkIKhoUZeqGTurdyjhrv9T71wEInI";
-
+const url1 = "https://maps.googleapis.com/maps/api/directions/json?origin=";
+const url2 ="&destination=";
+const url3 = "&key=AIzaSyBz6IDkIKhoUZeqGTurdyjhrv9T71wEInI";
 
 const getDirections = async (req, res, next) => {
     
     try {
-        const response = await axios.get(url);
+        //const data = req.body;
+        let origin = req.body.origin;
+        let destination = req.body.destination;
+        var res1 = origin.split(" ");
+        var res2 = destination.split(" ");
+        origin = res1.join("+");
+        destination = res2.join("+");
+        let URLCON = url1 + origin + url2 + destination + url3;
+        
+      
+        const response = await axios.get(URLCON);
+        
         let results = response.data.routes[0].legs[0].steps;
-          await firestore.collection('steps').doc().set(Object.assign({}, results));
+        
+        await firestore.collection('steps').doc().set(Object.assign({}, results));
           res.send('Recorded');   
+    } catch (error) {
+        res.status(400).send(error.message);
+        
+    }
+}
+const getLatLng = async (req, res, next) => {
+    
+    try {
+       
+
+        let adress = req.body.adress;
+        
+        const urlFinal = "https://maps.googleapis.com/maps/api/geocode/json?address="+adress+url3;
+        
+        const response = await axios.get(urlFinal);
+        let results = response.data.results[0].geometry.location;
+       
+          res.send(results);   
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+const getAddress = async (req, res, next) => {
+    
+    try {
+       
+
+        let lat = req.body.lat;
+        let lng = req.body.lng;
+        
+        const urlFinal = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+url3;
+        
+        const response = await axios.get(urlFinal);
+        let results = response.data.results[0].formatted_address;
+       
+          res.send(results);   
     } catch (error) {
         res.status(400).send(error.message);
     }
 }
 module.exports = {
     getDirections,
+    getLatLng,
+    getAddress
 }
--- routes/directions-routes.js
+++ routes/directions-routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const {getDirections
       } = require('../controllers/directionsController');
+const {getLatLng
+      } = require('../controllers/directionsController');
+const {getAddress
+      } = require('../controllers/directionsController');
 
 const router = express.Router();
 
 router.post('/direction', getDirections);
 
+router.post('/geocodeadd', getLatLng);
+router.post('/geocodeRev', getAddress);
 
 module.exports = {
     routes: router
