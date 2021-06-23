0f94246d82dd574231e286e1d2b8dd4e20687b96
el ouadi abdelati <Elouadi_Abdelati@outlook.fr>
2021-06-23 10:12:31 +0100

    fix typo

--- controllers/distancematrixContorller.js
+++ controllers/distancematrixContorller.js
@@ -1,5 +1,5 @@
 'use strict';
-const axios = require('axios'); 
+const axios = require('axios');
 const firebase = require('../db.js');
 const firestore = firebase.firestore();
 const DistanceMatrix = require('../models/distancematrix');
@@ -7,10 +7,10 @@ const request = require('request');
 const urlFirst = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=";
 const urlEnd = "&mode=walking&language=fr-FR&key=AIzaSyBz6IDkIKhoUZeqGTurdyjhrv9T71wEInI";
 
-const getTester = async (req, res, next) => {
-    
+const getTester = async(req, res, next) => {
+
     try {
-        
+
         let originLat = req.body.originLat;
         let originLng = req.body.originLng;
         let nextStepId = req.body.nextStepId;
@@ -20,22 +20,22 @@ const getTester = async (req, res, next) => {
         const data = await trip.get();
         const array = data.data();
         const DestinationLat = array.steps[nextStepId].start_location.lat;
-        
+
         res.send(DestinationLat);
     } catch (error) {
         res.status(400).send(error.message);
-        
+
     }
 }
-const getDistanceDuration = async (req, res, next) => {
-    
+const getDistanceDuration = async(req, res, next) => {
+
     try {
-        
+
         let originLat = req.body.originLat;
         let originLng = req.body.originLng;
         let nextStepId = req.body.nextStepId;
         let id = req.body.id;
-       console.log("sdcsdcsdcsdc ",originLat, originLng, nextStepId ,id);
+        console.log("sdcsdcsdcsdc ", originLat, originLng, nextStepId, id);
         const trip = await firestore.collection('steps').doc(id);
         const data = await trip.get();
         const array = data.data();
@@ -44,10 +44,10 @@ const getDistanceDuration = async (req, res, next) => {
         const DestinationFinalLat = array.start_location.lat;
         const DestinationFinalLng = array.start_location.lng;
         const maneuver = array.steps[nextStepId].maneuver;
-        const urlFinal = urlFirst+originLat+","+originLng+"&destinations="+DestinationLat+","+DestinationLng+urlEnd;
-        const urlFinal2 = urlFirst+originLat+","+originLng+"&destinations="+DestinationFinalLat+","+DestinationFinalLng+urlEnd;
+        const urlFinal = urlFirst + originLat + "," + originLng + "&destinations=" + DestinationLat + "," + DestinationLng + urlEnd;
+        const urlFinal2 = urlFirst + originLat + "," + originLng + "&destinations=" + DestinationFinalLat + "," + DestinationFinalLng + urlEnd;
         const response = await axios.get(urlFinal);
-        
+
         const ditanceText = response.data.rows[0].elements[0].distance.text;
         const durationText = response.data.rows[0].elements[0].duration.text;
         const ditanceValue = response.data.rows[0].elements[0].distance.value;
@@ -58,15 +58,17 @@ const getDistanceDuration = async (req, res, next) => {
         const distanceValueToEnd = response2.data.rows[0].elements[0].distance.value;
         const durationValueToEnd = response2.data.rows[0].elements[0].duration.value;
         let change = false;
-        if (durationValue<=30) change = true;
-        
-        const dMatrix = new DistanceMatrix(durationText,durationValue,ditanceText,ditanceValue,change,maneuver,distanceTextToEnd,durationTextToEnd,distanceValueToEnd,durationValueToEnd);
+        if (durationValue <= 30) change = true;
+
+        const dMatrix = new DistanceMatrix(durationText, durationValue, ditanceText, ditanceValue, change, maneuver, distanceTextToEnd, durationTextToEnd, distanceValueToEnd, durationValueToEnd);
         res.send(dMatrix);
     } catch (error) {
         res.status(400).send(error.message);
-        
+
     }
 }
+
+
 module.exports = {
     getDistanceDuration,
     getTester
